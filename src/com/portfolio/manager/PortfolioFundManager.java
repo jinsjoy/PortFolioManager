89ae573982cfbe4db8fe706e743c8714fc390e27
Jins Joy <jinsjoy@Jinss-MacBook-Air.local>
2019-08-05 00:51:16 +1000

    changed manager class to accept inout text file

--- src/com/portfolio/manager/PortfolioFundManager.java
+++ src/com/portfolio/manager/PortfolioFundManager.java
@@ -19,16 +19,16 @@ public class PortfolioFundManager {
 
 		PortfolioWeightCalculatorInterface fundGraph = new PortfolioFundWeightCalculator();
 
-//		if (args.length < 1) {
-//			System.out.println("Error:Please run with a input text file as parameter");
-//			System.exit(1);
-//		}
+		if (args.length < 1) {
+			System.out.println("Error:Please run with a input text file as parameter");
+			System.exit(1);
+		}
 
 		try {
 
-			File file = new File("/Users/jinsjoy/Documents/file.txt");
-			Scanner reader = new Scanner(file);
-			//Scanner reader = new Scanner(new FileInputStream(args[0]));
+			// File file = new File("/Users/jinsjoy/Documents/file.txt");
+			// Scanner reader = new Scanner(file);
+			Scanner reader = new Scanner(new FileInputStream(args[0]));
 			while (reader.hasNextLine()) {
 				String[] tokens = reader.nextLine().split("\\,");
 				fundGraph.addEdge(tokens[0], tokens[1], Integer.parseInt(tokens[2]));
@@ -39,13 +39,12 @@ public class PortfolioFundManager {
 		}
 
 		List<String> fundweights = fundGraph.fundWeightCalculator();
-		
+
 		System.out.println("List of Fund Weight for each node");
 		fundweights.stream().forEach(System.out::println);
-	
+
 		List<String> weightedReturnList = fundGraph.fundWeightedReturn();
 
-		
 		System.out.println("List of Weighted Return for each node");
 		weightedReturnList.stream().forEach(System.out::println);
 	}
